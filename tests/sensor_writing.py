eb460ecd17dd59df76e9407e3bab76233108b97f
Loic Banet <banet_l@yaka.epita.fr>
2017-11-04 17:40:45 +0100

    tests: fix writing

--- tests/sensor_writing.py
+++ tests/sensor_writing.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 
+import os
 import sys
 import serial
 
 from cobs import cobs
 
-sys.path.append('../python')
+sys.path.append('{}/../python'.format(os.path.dirname(sys.argv[0])))
 
 import sensor_pb2 as spb
 
@@ -33,7 +34,10 @@ def writeDummyReading(co2, temp, humid):
 with serial.Serial(port, baud, timeout=None) as ser:
     data1 = cobs.encode(writeDummyReading(30, 24.4, 12.8))
     ser.write(data1)
+    ser.write(b'\0')
     data2 = cobs.encode(writeDummyReading(45, 22.4, 19.8))
     ser.write(data2)
+    ser.write(b'\0')
     data3 = cobs.encode(writeDummyReading(11, 12.6, 90.2))
-    ser.write(data2)
+    ser.write(data3)
+    ser.write(b'\0')
